e56f321df0aed428c632db63f74eb68a6ecde399
nihad <nihadmoosakoya@gmail.com>
2025-10-14 19:14:42 +0530

    'first'

--- src/Services/baseUrl.js
+++ src/Services/baseUrl.js
@@ -1,3 +1,3 @@
 // const base_url="http://localhost:4000"
-const base_url="https://petshop-server-dnu3.onrender.com"
+const base_url="https://petshop-server-74ow.onrender.com"
 export default base_url
