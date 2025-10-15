4e2a2295005bfbfc82dcf9e9e9d17c73bc69bcb7
nihad <nihadmoosakoya@gmail.com>
2025-10-15 06:11:00 +0530

    'second'

--- src/User/PetcareVideo.jsx
+++ src/User/PetcareVideo.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
 
 function PetcareVideo() {
+  const videos = [
+    "https://www.youtube.com/embed/P3PWkQjab5I?si=DVHmGI2YbCoq6mCx",
+    "https://www.youtube.com/embed/q7hyE0t0Mfw?si=aTIZUNXd3njkWz3D",
+    "https://www.youtube.com/embed/jFMA5ggFsXU?si=aYzMWQ0kJy4S6gfx",
+    "https://www.youtube.com/embed/odoOvcvBMIo?si=jyc3npn6UW9M_MYG",
+    "https://www.youtube.com/embed/DbNNXpeGC7g?si=CGk0k5a05N42EOZ-",
+  ];
+
   return (
-  
     <>
-    <Header/>
-    <div className="container-fluid" style={{minHeight:"70vh", backgroundColor:"white"}}>
-      <h3 className='text-center py-5 text-primary'>Learn How to Care for Your Pet Like a Pro</h3>
-      <div className="d-flex justify-content-around" data-aos="fade-up-right">
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/P3PWkQjab5I?si=DVHmGI2YbCoq6mCx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/q7hyE0t0Mfw?si=aTIZUNXd3njkWz3D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/jFMA5ggFsXU?si=aYzMWQ0kJy4S6gfx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/odoOvcvBMIo?si=jyc3npn6UW9M_MYG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/DbNNXpeGC7g?si=CGk0k5a05N42EOZ-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <Header />
+      <div
+        className="container-fluid py-5"
+        style={{ minHeight: "70vh", backgroundColor: "white" }}
+      >
+        <h3 className="text-center pb-4 text-primary">
+          Learn How to Care for Your Pet Like a Pro
+        </h3>
+
+        <div className="container">
+          <div className="row g-4" data-aos="fade-up-right">
+            {videos.map((url, index) => (
+              <div
+                key={index}
+                className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center"
+              >
+                <div className="ratio ratio-16x9" style={{ width: "100%" }}>
+                  <iframe
+                    src={url}
+                    title={`Pet care video ${index + 1}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-    
     </>
-  )
+  );
 }
 
-export default PetcareVideo
+export default PetcareVideo;
